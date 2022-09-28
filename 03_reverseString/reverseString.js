6307667a974e3c569ec82ab1cc94d2cd20273fe8
DNUVision <davidfischsohn@gmail.com>
2022-09-28 16:49:32 +0300

    exercises 01-03 solved

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,21 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    // initializing empty array and empty string
+    let mediumArr = []; 
+    let reverseString = "";
 
+    //looping over the string paramter and pushing its letters to the array
+    for(let i = 0; i < string.length; i++){
+        mediumArr.push(string[i]);
+    }
+
+    //reversing the array's order
+    mediumArr = mediumArr.reverse();
+
+    //looping over the array and assigning its items to the empty string and returning it
+    for(let item = 0; item < mediumArr.length; item++){
+        reverseString += mediumArr[item];
+    }
+    return reverseString;
 };
 
 // Do not edit below this line
