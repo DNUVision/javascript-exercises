3798c3cc2ced57397fd5e677f1087634596349e6
DNUVision <davidfischsohn@gmail.com>
2022-10-12 11:42:03 +0300

    last exercise done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) * 5/9;
+  if(Number.isInteger(celsius)){
+      return celsius;
+  }else{
+      return Number(celsius.toFixed(1));
+  }
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  let fahrenheit = celsius * (9/5) + 32;
+  if(Number.isInteger(fahrenheit)){
+      return fahrenheit;
+  }else{
+      return Number(fahrenheit.toFixed(1));
+  }
 };
 
 // Do not edit below this line
