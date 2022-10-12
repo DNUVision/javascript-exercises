ce05ac786e6344f192c914c19ffc025dbd987313
DNUVision <davidfischsohn@gmail.com>
2022-10-12 10:47:01 +0300

    another batch of exercises being solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...item) {
+    console.log('this is item provided ' + item);
+    for(let i = 0; i < item.length; i++){
+        for(let j = 0; j < arr.length; j++){
+            if(arr[j] === item[i]){
+                arr.splice(j,1);
+            }
+        }
+        }
+    return arr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let iter;
+    let iterable;
+    let sum = 0;
+    if(typeof num1 !== "number" || typeof num2 !== "number"){
+        return "ERROR";
+    }else if(0 > num1 || 0 > num2){
+        return "ERROR";
+    }else{
+        if(num1 > num2){
+            iter = num2;
+            iterable = num1;
+        }else{
+            iter = num1;
+            iterable = num2;
+        }
+    }
+    
+    for(let i = iter; i <= iterable; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
